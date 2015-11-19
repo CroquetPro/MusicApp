1a2aac24cd833e40e082948994ec181919a2d9d6
Ben Rothman <croquetpro@gmail.com>
2015-11-19 12:47:42 -0800

    Users and Sessions work

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password])
+
+    if user.nil?
+      flash.now[:errors] = ["Incorrect Email or password."]
+      render :new
+    else
+      login!(user)
+      redirect_to user_url(user)
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
+
+  def new
+    @user = User.new
+  end
+
+end
